afb44cdf1fd701b96ef20c8d524194ed39122d65
gcwill70 <gcwill70@gmail.com>
2023-02-10 13:47:48 -0500

    update dependencies

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from datetime import datetime
 from actions_toolkit import core
 from github import Github
 
+
 def search_repos(args):
     g = Github(args['token'])
     repos = g.get_organization(args['org']).get_repos()
-    matched = []; remaining = repos.totalCount; page_num = 0
+    matched = []
+    remaining = repos.totalCount
+    page_num = 0
     while remaining > 0:
         # get next page
         page_results = repos.get_page(page_num)
@@ -17,7 +20,7 @@ def search_repos(args):
         for repo in page_results:
             if re.match(args['pattern'], repo.name) != None:
                 if repo.created_at >= args['created_after'] and \
-                repo.created_at <= args['created_before']:
+                        repo.created_at <= args['created_before']:
                     matched.append(repo.full_name)
         # update indeces
         remaining -= len(page_results)
@@ -26,21 +29,27 @@ def search_repos(args):
         raise Exception(f'No repositories found.\nargs = {args}')
     return matched
 
+
 def parse_env():
     inputs = ['org', 'token', 'pattern', 'created_after', 'created_before']
     args = {}
     for input in inputs:
-        args[input] = core.get_input(input, trim_whitespace = False)
-    args['created_after'] = datetime.strptime(args['created_after'], '%d/%m/%Y %H:%M:%S')
-    args['created_before'] = datetime.strptime(args['created_before'], '%d/%m/%Y %H:%M:%S')
-    if args['created_after'] >= args['created_before']: raise Exception('created_after must come before created_before.')
+        args[input] = core.get_input(input, trim_whitespace=False)
+    args['created_after'] = datetime.strptime(
+        args['created_after'], '%d/%m/%Y %H:%M:%S')
+    args['created_before'] = datetime.strptime(
+        args['created_before'], '%d/%m/%Y %H:%M:%S')
+    if args['created_after'] >= args['created_before']:
+        raise Exception('created_after must come before created_before.')
     return args
+
 
 def set_default_env():
     os.environ.setdefault('INPUT_CREATED_AFTER', '01/01/1970 00:00:00')
     if os.environ.get('INPUT_CREATED_BEFORE', '') == '':
-        os.environ['INPUT_CREATED_BEFORE'] = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-    os.environ.setdefault('INPUT_TOKEN', os.environ.get('GITHUB_TOKEN', ''))
+        os.environ['INPUT_CREATED_BEFORE'] = datetime.now().strftime(
+            '%d/%m/%Y %H:%M:%S')
+
 
 def main():
     try:
@@ -51,6 +60,7 @@ def main():
         core.info(f'{json.dumps(matched)}')
     except Exception as e:
         core.set_failed(str(e))
-        
+
+
 if __name__ == "__main__":
     main()
--- test_unit/test_firebase.py
+++ test_unit/test_firebase.py
@@ -2,9 +2,10 @@ import os
 from main import main
 from unittest import mock
 
+
 @mock.patch.dict(os.environ, {
     "INPUT_ORG": "firebase",
     "INPUT_PATTERN": '^quickstart.*$'
-    })
+})
 def test_firebase():
     main()
